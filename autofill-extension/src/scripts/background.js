5b11a0f668cea5e486f0d9940eb8ad15ac89d51c
natleo <n8wyliu@gmail.com>
2026-04-09 13:31:09 -0700

    add top header comment

--- autofill-extension/src/scripts/background.js
+++ autofill-extension/src/scripts/background.js
@@ -5,6 +5,7 @@
  * 1. When a Chrome user clicks on our panel in the extensions tab,
  *    it opens our side panel.
  */
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
 });
